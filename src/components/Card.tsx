bf060977178df77b1d04c3343cb1465031da4cba
Ben Kogan <brk57@cornell.edu>
2022-06-16 19:38:57 -0400

    card adjust

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from "react";
 const Card: React.FC<PropsWithChildren<{}>> = (props) => {
   return (
     <div className="flex justify-center items-center h-screen w-screen">
-      <div className="rounded overflow-hidden drop-shadow-2xl bg-slate-800 p-10 flex flex-col justify-center h-1/4 mx-14">
+      <div className="rounded overflow-hidden drop-shadow-2xl bg-slate-800 px-10 py-5 flex flex-col justify-center h-auto mx-14">
         {props.children}
       </div>
     </div>
