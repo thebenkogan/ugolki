7c50289b1d73487eae2c12594408e7f3b2ea3af0
Ben Kogan <brk57@cornell.edu>
2022-06-27 01:02:19 -0400

    fix board border color

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -67,12 +67,11 @@ function Board({
     }
   };
 
+  const border = isTurn ? "border-teal-500" : "border-black";
   return (
     <div className="flex justify-center items-center h-screen w-screen">
       <div
-        className={`grid grid-cols-8 grid-rows-8 w-screen sm:max-w-xl mx-5 border-4 sm:border-8 border-${
-          isTurn ? "teal-500" : "black"
-        }`}
+        className={`grid grid-cols-8 grid-rows-8 w-screen sm:max-w-xl mx-5 border-4 sm:border-8 ${border}`}
       >
         {game.board.flatMap((row, y) =>
           row.map((player, x) => {
